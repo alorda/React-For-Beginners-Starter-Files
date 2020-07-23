552c8ed6f309e35b8a109220ac8cc1da1ca2e4b5
Jonathan Alorda <jonathan.alorda@gmail.com>
2020-07-22 23:13:43 -0500

    making a commit before we learn about React Router.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,13 @@ import { render } from 'react-dom'
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Je suis le StorePicker</p>
+    return (
+      <form action="" className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
